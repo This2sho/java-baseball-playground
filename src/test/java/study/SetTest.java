0d7684912dc97cabbed9d7e218fb86afee111b61
This2sho <zx00018@naver.com>
2022-10-27 00:30:47 +0900

    test: 요구사항 3
    요구사항 2에서 true, false의 경우를 나눠서 CsvSource 어노테이션으로 구현

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetTest {
@@ -34,5 +35,11 @@ public class SetTest {
 	@ValueSource(ints = {1, 2, 3})
 	public void setContainsTest(int number) {
 		assertThat(numbers.contains(number)).isTrue();
+	}
+
+	@ParameterizedTest
+	@CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+	public void setContainsTrueOrFalseTest(int input, boolean expected) {
+		assertThat(numbers.contains(input)).isEqualTo(expected);
 	}
 }

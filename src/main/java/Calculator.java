02e215b998c0df37b1b0d8a18283c7b72dda830b
This2sho <zx00018@naver.com>
2022-10-28 01:42:39 +0900

    feat: 계산기 유효성 검사 기능 구현

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -24,16 +25,35 @@ public class Calculator {
 			addNumber(input);
 			return false;
 		}
-
 		addOperator(input);
 		return true;
 	}
 
 	private void addNumber(String number) {
-		numbers.add(Integer.parseInt(number));
+		validateNumber(number);
+		int num = Integer.parseInt(number);
+		numbers.add(num);
+	}
+
+	private void validateNumber(String number) {
+		try {
+			Integer.parseInt(number);
+		} catch (NumberFormatException e) {
+			throw new IllegalStateException("잘못된 형식의 입력입니다.");
+		}
 	}
 
 	private void addOperator(String operator) {
-		operators.add(operator.charAt(0));
+		char op = operator.charAt(0);
+		validateOperator(op);
+		operators.add(op);
+	}
+
+	private void validateOperator(char op) {
+		char[] OPERATORS = {'+', '-', '*', '/'};
+		boolean doContain = Arrays.asList(OPERATORS).contains(op);
+		if (!doContain) {
+			throw new IllegalStateException("잘못된 형식의 입력입니다.");
+		}
 	}
 }
